bedd9e94e03b208af9e55e93b337c93872cd7c1c
zhen.chen <zhen.chen@itrigger.com>
2020-07-07 10:10:15 +0800

    路由机制的简单实现

--- server/index.js
+++ server/index.js
@@ -2,27 +2,49 @@
 import React from 'react'
 import {renderToString} from 'react-dom/server'
 import express from 'express'
+import {StaticRouter} from 'react-router-dom'
 import App from '../src/App'
 
 const app = express()
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-    // const Page = <App title='努力学习'></App>
-    // 把react组件，解析成html
-    const content = renderToString(App)
+// app.get('/', (req, res) => {
+//     // const Page = <App title='努力学习'></App>
+//     // 把react组件，解析成html
+//     const content = renderToString(App)
+//     res.send(`
+//         <html>
+//             <head>
+//                 <meta chartset="utf-8" />
+//                 <title>react ssr</title>
+//             </head>
+//             <body>
+//                 <div id="root">${content}</div>
+//             </body>
+//             <script src="/bundle.js"></script>
+//         </html>
+//     `)
+// })
+
+// 路由
+app.get('*', (req, res) => {
+    const content = renderToString(
+        <StaticRouter location={req.url}>
+            {App}
+        </StaticRouter>
+    )
     res.send(`
-        <html>
-            <head>
-                <meta chartset="utf-8" />
-                <title>react ssr</title>
-            </head>
-            <body>
-                <div id="root">${content}</div>
-            </body>
-            <script src="/bundle.js"></script>
-        </html>
-    `)
+         <html>
+             <head>
+                 <meta chartset="utf-8" />
+                 <title>react ssr</title>
+             </head>
+             <body>
+                 <div id="root">${content}</div>
+             </body>
+             <script src="/bundle.js"></script>
+         </html>
+     `)
 })
 app.listen(9093, () => {
     console.log('监听完毕')
--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React, {useState} from 'react'
+// import React, {useState} from 'react'
+//
+// function App(props){
+//     const [count, setCount] = useState(1)
+//     return <div>
+//         <h1>跩跩珍 {props.title} {count}</h1>
+//         <button onClick={() =>setCount(count+1)}>累加</button>
+//     </div>
+// }
+//
+// export default <App title="跩跩珍"></App>
 
-function App(props){
-    const [count, setCount] = useState(1)
-    return <div>
-        <h1>跩跩珍 {props.title} {count}</h1>
-        <button onClick={() =>setCount(count+1)}>累加</button>
+// 抽里成入口文件
+import React from 'react'
+import {Route} from 'react-router-dom'
+import Index from './container/Index'
+import About from "./container/About"
+export default (
+    <div>
+        <Route path="/" exact component={Index}></Route>
+        <Route path="/about" exact component={About}></Route>
     </div>
-}
-
-export default <App title="跩跩珍"></App>
+)
